4d76fb8bb6eef7446da065b8d992bc3c81db3436
Madhav <madhav.chitlangia@gmail.com>
2024-05-06 16:34:59 +0530

    add app component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,3 +1,33 @@
-export default function App() {
-    return null;
+import React from "react";
+import Filters from "../Filters";
+import CardsContainer from "../CardsContainer";
+import {JobListingContext} from '../../context';
+
+const initialFilter = {
+  roles: [],
+  location: [],
+  experience: [],
+  remote: [],
+  minimum_base_pay_salary: [],
+  search: ""
 }
+
+function App() {
+  const [filters, setFilter] = React.useState(initialFilter);
+
+  return (
+      <JobListingContext.Provider
+        value={{
+          filters,
+          setFilter
+        }}
+      >
+        <div>
+          <Filters />
+          <CardsContainer />
+        </div>
+      </JobListingContext.Provider>
+  );
+}
+
+export default App;
